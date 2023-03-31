33aad775f5fdf6b4f75770022abfb5af5dc15ec1
Damien Pellerin <damienpellerin19@gmail.com>
2023-03-31 14:52:06 +0200

    API + FONTS

---
+++ App.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  img_background: {
+    flex: 1,
+  },
+  img: {
+    opacity: 0.75,
+  },
+});
+
+export { s };
