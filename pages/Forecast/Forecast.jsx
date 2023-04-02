0cd520891a6c9ef70f1cfb4719bc0384eaebcb12
Damien Pellerin <damienpellerin19@gmail.com>
2023-04-02 09:48:18 +0200

    Prev 7 Days

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -3,13 +3,15 @@ import { Txt } from "../../components/Txt/Txt";
 import { TouchableOpacity, View } from "react-native";
 import { s } from "./Forecast.style";
 import { useNavigation, useRoute } from "@react-navigation/native";
-
+import { ForecastListItem } from "../../components/ForecastListItem/ForecastListItem";
+import { getWeatherInterpretation } from "../../services/meteo-service";
+import { dateToDDMM, DAYS } from "../../services/date-service";
 export function Forecast({}) {
   const { params } = useRoute();
   const { nav } = useNavigation();
 
   const backButton = (
-    <TouchableOpacity onPress={() => nav.goBack()}>
+    <TouchableOpacity style={s.back_btn} onPress={() => nav.goBack()}>
       <Txt>{"<"}</Txt>
     </TouchableOpacity>
   );
@@ -17,11 +19,38 @@ export function Forecast({}) {
   const header = (
     <View style={s.header}>
       {backButton}
-      <View>
-        <Txt style={s.header_texts}></Txt>
+      <View style={s.header_texts}>
+        <Txt>{params.city}</Txt>
         <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
       </View>
     </View>
   );
-  return <Container>{backButton}</Container>;
+
+  const forecastList = (
+    <View style={s.forecastList}>
+      {params.time.map((time, index) => {
+        const code = params.weathercode[index];
+        const image = getWeatherInterpretation(code).image;
+        const date = new Date(time);
+        const day = DAYS[date.getDay()];
+        const temperature = params.temperature_2m_max[index];
+        return (
+          <ForecastListItem
+            image={image}
+            day={day}
+            key={time}
+            date={dateToDDMM(date)}
+            temperature={temperature.toFixed(0)}
+          />
+        );
+      })}
+    </View>
+  );
+
+  return (
+    <Container>
+      {header}
+      {forecastList}
+    </Container>
+  );
 }
